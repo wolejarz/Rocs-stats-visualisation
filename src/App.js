7431b199b6b9775085456b48b5b6a66b8fce6945
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-07 20:00:04 +0200

    Added responsive design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 function App() {
+  const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  }));
+  const showImg = () => <img src="https://plotly.com/~M1kol4j/21898.png?share_key=qut1EDishqKWlv0plN4fwW" alt="vilje_cost_per_user"  width="600" onerror="this.onerror=null;this.src='https://plotly.com/404.png';" />
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Grid container spacing={1}>
+         <Grid item sm={12} md={6} lg={4}>
+          <Item>{showImg()}</Item>
+        </Grid>
+         <Grid item sm={12} md={6} lg={4}>
+          <Item>{showImg()}</Item>
+        </Grid>
+        <Grid item sm={12} md={6} lg={4}>
+          <Item>{showImg()}</Item>
+        </Grid>
+        <Grid item sm={12} md={6} lg={4}>
+          <Item>{showImg()}</Item>
+        </Grid>
+        <Grid item sm={12} md={6} lg={4}>
+          <Item>{showImg()}</Item>
+        </Grid>
+        <Grid item sm={12} md={6} lg={4}>
+          <Item>{showImg()}</Item>
+        </Grid>
+      
+  
+</Grid>
     </div>
   );
 }
