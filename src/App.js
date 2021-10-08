26f077f4d4521c165242fd1548d33fb11558500b
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-08 09:17:05 +0200

    Added some spacing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
 function App() {
+  
   const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
   }));
+
   const showImg = () => <img src="https://plotly.com/~M1kol4j/21898.png?share_key=qut1EDishqKWlv0plN4fwW" alt="vilje_cost_per_user"  width="600" onerror="this.onerror=null;this.src='https://plotly.com/404.png';" />
 
   
