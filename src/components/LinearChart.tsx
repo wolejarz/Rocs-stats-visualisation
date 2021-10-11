849c41a2258401e3768d3efe8092d1c932f4517d
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-11 13:55:57 +0200

    Added LinearChart Component

---
+++ src/components/LinearChart.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface IProps {
+  url: string;
+}
+
+export default class LinearChart extends React.Component<IProps> {
+  render() {
+    return <span></span>;
+  }
+}
