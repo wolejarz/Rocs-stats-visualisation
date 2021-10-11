6ebfacee51790509df43165e29c07d79825286c7
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-11 14:31:28 +0200

    Added implementation of component

--- src/components/LinearChart.tsx
+++ src/components/LinearChart.tsx
@@ -1,11 +1,43 @@
 import React from "react";
+import Chart from "react-google-charts";
 
 interface IProps {
   url: string;
 }
 
-export default class LinearChart extends React.Component<IProps> {
+class LinearChart extends React.Component<IProps> {
+  state = { dataLoadingStatus: "loading", chartData: [] };
+  didMount = async function (component) {
+    const response = await fetch("https://api.exchangeratesapi.io/latest?symbols=USD,GBP,CAD");
+    const json = await response.json();
+    const rateCurrencyNames = Object.keys(json.rates);
+    const rateCurrencyValues: string[] = Object.values(json.rates);
+    const chartData = [["Currency Name", "Currency Rate"]];
+    for (let i = 0; i < rateCurrencyNames.length; i += 1) {
+      chartData.push([rateCurrencyNames[i], rateCurrencyValues[i]]);
+    }
+    this.setState({
+      dataLoadingStatus: "ready",
+      chartData: chartData
+    });
+  };
   render() {
-    return <span></span>;
+    return this.state.dataLoadingStatus === "ready" ? (
+      <Chart
+        chartType="BarChart"
+        data={this.state.chartData}
+        options={{
+          chartArea: {
+            width: "50%"
+          },
+          title: "EUR Price"
+        }}
+        rootProps={{ "data-testid": "1" }}
+      />
+    ) : (
+      <div>Fetching data from API</div>
+    );
   }
 }
+
+export default LinearChart;
