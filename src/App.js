38bc21708a97a7b9ab54bcd0908acaf72053d285
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-11 11:39:21 +0200

    Left only one chart for testing

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ function App() {
   
   const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
-    width: `800px`,
+    width: `1600px`,
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
   }));
 
   const showImg = (url, title) => <Chart
-    width={'800px'}
-    height={'650px'}
+    width={'1600px'}
+    height={'1000px'}
     chartType="LineChart"
     spreadSheetUrl={url}
     options={{
@@ -33,38 +33,12 @@ function App() {
   return (
     <div className="App">
       <Grid container spacing={1}>
-        <Grid item  lg={12} xl={6}>
+        <Grid item  lg={12} xl={12}>
          <Button width="200" variant="outlined"><h2>Vilje</h2></Button>
         </Grid>
-        <Grid item lg={12} xl={6}>
-          <Button variant="outlined"><h2>Fram</h2></Button>
-        </Grid>
-        <Grid item  lg={12} xl={6}>
+        <Grid item  lg={12} xl={12}>
           <Item>{showImg("https://docs.google.com/spreadsheets/d/1ZL4Jv61ODAufPLiC-ssOy4Yqzq7jYbLi4tqXH1uHIO8/edit?usp=sharing","Cost per user")}</Item>
         </Grid>
-        <Grid item  lg={12} xl={6}>
-          <Item>{showImg("https://docs.google.com/spreadsheets/d/1eDZ6E0fMx147tG-uEI2fymvSYbh_Uq58fqNoocmXnUo/edit?usp=sharing","Cost per user")}</Item>
-        </Grid>
-
-        <Grid item  lg={12} xl={6}>
-          <Item>{showImg("https://docs.google.com/spreadsheets/d/13fzV3SpaDRSUNtol3zFI9g4uFQBtgghXx3bQiQXizP0/edit?usp=sharing","Running tasks per user")}</Item>
-        </Grid>
-        <Grid item  lg={12} xl={6}>
-          <Item>{showImg("https://docs.google.com/spreadsheets/d/11zsbU-pxub5AfNs00Gy3zkw6InI29-VD0Txy9WyYL7U/edit?usp=sharing","Running tasks per user")}</Item>
-        </Grid>
-         <Grid item  lg={12} xl={6}>
-          <Item>{showImg("https://docs.google.com/spreadsheets/d/1aM-Y2ibpgEGJKRzp-osmu0loPqsJmh-2yDS8fKVytzQ/edit?usp=sharing","Queuing tasks per user")}</Item>
-        </Grid>
-        <Grid item  lg={12} xl={6}>
-          <Item>{showImg("https://docs.google.com/spreadsheets/d/1F--Mw0JMRilA4hyQENiD9dkYt25efxAwWIn4ipefRww/edit?usp=sharing","Queuing tasks per user")}</Item>
-        </Grid>
-        <Grid item  lg={12} xl={6}>
-          <Item>{showImg("https://docs.google.com/spreadsheets/d/1z5_FkSoOk9vhoFktOulf05baN69IAoi5x-ItQnfNaBI/edit?usp=sharing","R vs Q")}</Item>
-        </Grid>
-         <Grid item  lg={12} xl={6}>
-          <Item>{showImg("https://docs.google.com/spreadsheets/d/12Mt9Py47qsDVmK-h98uV0rNolzDPYY9WgalIF_l4g0c/edit?usp=sharing","R vs Q")}</Item>
-        </Grid>
-  
 </Grid>
     </div>
   );
