12dd1f1ff711626c7aecb2cca304f2cdd9886ffa
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-11 20:13:17 +0200

    Some changes

--- src/utilities/Utilities.tsx
+++ src/utilities/Utilities.tsx
@@ -1,1 +1,8 @@
-export const dataFromCSV = url => {};
+import csv from "csv-parser";
+
+export const dataFromCSV = async url => {
+  const results: any[] = [];
+  const response = await fetch(url, { mode: "no-cors" });
+  const body = await response.text();
+  console.log("body", body, response);
+};
