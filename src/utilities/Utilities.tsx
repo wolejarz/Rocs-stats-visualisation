4ef0b7d57a09f82173770de03694fe8383f5f95c
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-12 11:43:34 +0200

    Added conversion text values to typed

--- src/utilities/Utilities.tsx
+++ src/utilities/Utilities.tsx
@@ -1,7 +1,27 @@
 import Papa from "papaparse";
+
 export const dataFromCSV = async url => {
   const response = await fetch("http://localhost/vilje.hpc.ntnu.no.cost.csv");
   const body = await response.text();
   const values = await Papa.parse(body);
-  console.log("Array of values", values);
+  const typedData: any[] = [];
+  values.data.forEach((dataRow, index) => {
+    if (dataRow.length <= 1) return;
+    if (index === 0) {
+      dataRow[0] = { type: "date", label: "Time" };
+      typedData.push(dataRow);
+      return;
+    }
+    typedData.push(
+      dataRow.map(value => {
+        if (!isNaN(Number(value))) {
+          return Number(value);
+        }
+        return new Date(value);
+      })
+    );
+  });
+  console.log("Data and other values", values.data, typedData[0], typedData[1]);
+
+  return typedData;
 };
