9607df2fc376b20eba48c832d3f98ed098870cf3
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-12 13:03:33 +0200

    Added justifing of grids

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ function App() {
 
   return (
     <div className="App">
-      <Grid container spacing={1}>
+      <Grid container spacing={2} justify="center">
         <Grid item  lg={12} xl={12}>
          <Button width="200" variant="outlined"><h2>Vilje</h2></Button>
         </Grid>
-        <Grid item lg={12} xl={12}>
+        <Grid item lg={12} xl={12} align="center" >
           <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.cost.csv"} title={"COST"}/>
         </Grid>
-        <Grid item lg={12} xl={12}>
+        <Grid item lg={12} xl={12} align="center">
           <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.running.csv"} title={"RUNNING"}/>
         </Grid>
-        <Grid item lg={12} xl={12}>
+        <Grid item lg={12} xl={12} align="center">
           <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.queuing.csv"} title={"QUEUING"}/>
         </Grid>
-        <Grid item lg={12} xl={12}>
+        <Grid item lg={12} xl={12} align="center">
           <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.summary.csv"} title={"SUMMARY"}/>
         </Grid>
       </Grid>
