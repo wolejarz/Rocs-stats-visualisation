22e41e75e33f8c7800d72eef1eb1134f7392a0b6
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-12 12:36:28 +0200

    Some hHTML code moved to component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import LinearChart from "./components/LinearChart";
 
-
 function App() {
-  
-  const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    width: `1200px`,
-    margin: 'auto',
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  }));
-
-  const showImg = (url, title) =>
-    <LinearChart url={url} title={title}/>
 
   return (
     <div className="App">
@@ -26,19 +11,19 @@ function App() {
         <Grid item  lg={12} xl={12}>
          <Button width="200" variant="outlined"><h2>Vilje</h2></Button>
         </Grid>
-        <Grid item  lg={12} xl={12}>
-          <Item>{showImg("http://localhost/vilje.hpc.ntnu.no.cost.csv","COST")}</Item>
+        <Grid item lg={12} xl={12}>
+          <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.cost.csv"} title={"COST"}/>
         </Grid>
-        <Grid item  lg={12} xl={12}>
-          <Item>{showImg("http://localhost/vilje.hpc.ntnu.no.running.csv","RUNNUNG")}</Item>
+        <Grid item lg={12} xl={12}>
+          <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.running.csv"} title={"RUNNING"}/>
         </Grid>
-        <Grid item  lg={12} xl={12}>
-          <Item>{showImg("http://localhost/vilje.hpc.ntnu.no.queuing.csv","QUEUING")}</Item>
+        <Grid item lg={12} xl={12}>
+          <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.queuing.csv"} title={"QUEUING"}/>
         </Grid>
-        <Grid item  lg={12} xl={12}>
-          <Item>{showImg("http://localhost/vilje.hpc.ntnu.no.summary.csv","SUMMARY")}</Item>
+        <Grid item lg={12} xl={12}>
+          <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.summary.csv"} title={"SUMMARY"}/>
         </Grid>
-</Grid>
+      </Grid>
     </div>
   );
 }
