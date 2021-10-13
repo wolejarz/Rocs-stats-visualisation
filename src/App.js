95741074144594a399af9f9167a4f9d6d50b47ae
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-13 11:38:36 +0200

    Added tabbed nav bar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
 import './App.css';
+import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+
 import LinearChart from "./components/LinearChart";
 import PieChart from "./components/PieChart";
 
 
 function App() {
+  const [value, setValue] = React.useState(0);
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className="App">
       <Grid container spacing={2} justify="center">
-        <Grid item  lg={12} xl={12}>
+        <Grid item  lg={12} xl={12} align="center">
          <Button width="200" variant="outlined"><h2>Vilje</h2></Button>
         </Grid>
-        <Grid item lg={12} xl={12} align="center" >
+         
+
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} centered>
+              <Tab label="COST PER USER"  />
+            <Tab label="RUNNING TASKS PER USER"  />
+            <Tab label="QUEUING TASKS PER USER"  />
+              <Tab label="R vs Q" />
+          </Tabs>
+        </Box>
+      </Box>
+
+
           <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.cost.csv"} title={"COST"}/>
-        </Grid>
-        <Grid item lg={12} xl={12} align="center">
+       
           <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.running.csv"} title={"RUNNING"}/>
-        </Grid>
-        <Grid item lg={12} xl={12} align="center">
+       
           <LinearChart url={"http://localhost/vilje.hpc.ntnu.no.queuing.csv"} title={"QUEUING"}/>
-        </Grid>
-        <Grid item lg={12} xl={12} align="center">
+        
           <PieChart url={"http://localhost/vilje.hpc.ntnu.no.summary.csv"} title={"SUMMARY"}/>
-        </Grid>
+      
       </Grid>
     </div>
   );
