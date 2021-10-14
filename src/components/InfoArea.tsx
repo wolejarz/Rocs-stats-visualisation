35564d8bfcef123640faebc013f3810682720ed9
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-10-14 08:47:18 +0200

    Fetching markdown file form external source

---
+++ src/components/InfoArea.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Paper from "@mui/material/Paper";
+
+class InfoArea extends React.Component {
+  state = { dataLoadingStatus: "loading", infoData: "" };
+  async componentDidMount() {
+    const response = await fetch("http://localhost/infoarea.md");
+    const data = await response.text();
+    this.setState({
+      infoData: data,
+      dataLoadingStatus: "ready"
+    });
+  }
+
+  render() {
+    return this.state.dataLoadingStatus === "ready" ? (
+      <div>
+        <Paper
+          elevation={5}
+          style={{
+            width: "600px",
+            height: "500px"
+          }}
+        ></Paper>
+      </div>
+    ) : (
+      <div>Fetching data from API</div>
+    );
+  }
+}
+
+export default InfoArea;
